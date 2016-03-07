8affd144ed5641f776697bfcdd6959f2a3ca1938
septem1120 <Septem-Mac@huangguoxiongdeMacBook-Pro.local>
2016-03-08 00:04:16 +0800

    init

---
+++ Assets/Script/Common/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.Movement ();
+	}
+
+	void Movement(){
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Translate(Vector2.left * 4f * Time.deltaTime);
+			transform.eulerAngles = new Vector2(0, 180);
+		} 
+		else if (Input.GetKey (KeyCode.A)) {
+			transform.Translate(Vector2.left * 4f * Time.deltaTime);
+			transform.eulerAngles = new Vector2(0, 0);
+		}
+	}
+}
